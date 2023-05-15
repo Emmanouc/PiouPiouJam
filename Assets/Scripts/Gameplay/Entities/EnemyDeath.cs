3e37fc8b9bfcdd3c313a3ccfb7b0687f00108090
hchloe2001@hotmail.fr <hchloe2001@hotmail.fr>
2023-05-15 15:32:48 +0200

    fix

--- Assets/Scripts/Gameplay/Entities/EnemyDeath.cs
+++ Assets/Scripts/Gameplay/Entities/EnemyDeath.cs
@@ -6,6 +6,7 @@ public class EnemyDeath : MonoBehaviour
 {
     public GameObject DeathFX;
     public float delay;
+    public float ParticleLifetime;
 
 
     void Start()
@@ -15,12 +16,14 @@ public class EnemyDeath : MonoBehaviour
 
     private IEnumerator Delay(float delay)
     {
+        Debug.Log("coucou");
         yield return new WaitForSeconds(delay);
 
         GameObject Soul = Instantiate(DeathFX, transform.position, DeathFX.transform.rotation);
+        GameObject.Destroy(Soul, ParticleLifetime);
 
-        ParticleSystem SoulFX = Soul.GetComponent<ParticleSystem>();
-        yield return new WaitWhile(() =>SoulFX.isPlaying);
+
+        yield return new WaitForSeconds(ParticleLifetime);
 
         GameObject.Destroy(gameObject);
     }
