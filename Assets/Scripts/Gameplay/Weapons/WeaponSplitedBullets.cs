70470e054cc499ae217f72161a3d4a67fa5820b4
Emmanouc <edelautel@gmail.com>
2023-05-16 14:49:34 +0200

    FX

--- Assets/Scripts/Gameplay/Weapons/WeaponSplitedBullets.cs
+++ Assets/Scripts/Gameplay/Weapons/WeaponSplitedBullets.cs
@@ -6,13 +6,13 @@ namespace Gameplay.Weapons
     /// <summary>
     /// Represents a weapon that shot one bullet at a time to the closest enemy
     /// </summary>
-    public class WeaponBullet : WeaponBase
+    public class WeaponSplitedBullets : WeaponBase
     {
 
         [SerializeField] GameObject _prefab;
         [SerializeField] float _speed;
 
-        public WeaponBullet()
+        public WeaponSplitedBullets()
         {
         }
         
@@ -31,12 +31,14 @@ namespace Gameplay.Weapons
 
             var playerPosition = player.transform.position;
             GameObject go = GameObject.Instantiate(_prefab, playerPosition, Quaternion.identity);
+            GameObject go1 = GameObject.Instantiate(_prefab, playerPosition, Quaternion.identity);
             Vector3 direction = enemy.transform.position - playerPosition;
             if (direction.sqrMagnitude > 0)
             {
                 direction.Normalize();
 
                 go.GetComponent<Bullet>().Initialize(direction, GetDamage(),_speed);
+                go1.GetComponent<Bullet>().Initialize(-direction, GetDamage(), _speed);
             }
         }
     }
