0780c4f9135876c4c7a96ea03ac9813a55bba22e
hchloe2001@hotmail.fr <hchloe2001@hotmail.fr>
2023-05-16 16:32:34 +0200

    ghopygt

--- Assets/Scripts/Gameplay/Entities/EnemyController.cs
+++ Assets/Scripts/Gameplay/Entities/EnemyController.cs
@@ -84,8 +84,8 @@ public class EnemyController : Unit
         _sprite.DOColor(Color.red, 0).SetEase(Ease.OutElastic);
         _sprite.DOColor(Color.white, 1);
 
-        transform.DOComplete();
-        transform.DOShakePosition(0.4f, strength: 0.5f).SetEase(Ease.OutBounce);
+        //transform.DOComplete();
+        transform.DOShakePosition(0.3f, strength: 0.5f).SetEase(Ease.OutBounce);
 
         if (Life <= 0)
         {
